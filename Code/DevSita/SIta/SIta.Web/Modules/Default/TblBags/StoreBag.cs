44cf63cbbd4969826c6620ba7a4334709ae09d51
nvthang02 <nvthang02@ecpay.vn>
2020-09-17 22:17:18 +0700

    Thang add

--- Code/DevSita/SIta/SIta.Web/Modules/Default/TblBags/StoreBag.cs
+++ Code/DevSita/SIta/SIta.Web/Modules/Default/TblBags/StoreBag.cs
@@ -14,6 +14,7 @@ namespace Sita.Modules.Default.TblBags
 {
     using MyRepository = Sita.Default.Endpoints.TblBagsController;
     using MyRow = Sita.Default.Entities.TblBagsRow;
+    
     public class  StoreBag
     {
         public static void Save(string mess)
@@ -25,21 +26,40 @@ namespace Sita.Modules.Default.TblBags
             {
                 for (int i = 0; i < ObjectData.Length; i++)
                 {
-                    if (ObjectData[i].Substring(0, 1) == "N")
+
+
+                    if (ObjectData[i].Substring(0, 2) == "N/")
                     {
                         var newBagtag = new TblBagsRow();
                         var connection = SqlConnections.NewFor<MyRow>();
 
-                        newBagtag.BaggageTag = "";//Lấy chuổi 
+                        newBagtag.BaggageTag = ObjectData[i].Substring(2, 10);//Lấy chuổi 
+                        newBagtag.FlightRef = Array.Find(ObjectData, element => element.StartsWith("F/", StringComparison.Ordinal));
+                        newBagtag.Bsm = mess;
+                        newBagtag.Processed = true;
+                        newBagtag.TimeRcvBpm = DateTime.Now;
                         //.....
                         // Gán thêm các giá trị khác
                         //
-                        connection.Insert<MyRow>(newBagtag);
+                        try
+                        {
+                            UnitOfWork unitOfWork = new UnitOfWork(connection);
+                            SaveRequest<MyRow> saveRequest = new SaveRequest<MyRow>();
+                            saveRequest.Entity = newBagtag;
+                            new TblBagsController().Create(unitOfWork, saveRequest);
+                            //connection.Insert<MyRow>(newBagtag);
+                        }
+                        catch (Exception ex)
+                        {
+                            //Update lai
+                            
+                        }
                         
+
                     }
                 }
             }
-           
+
 
              //Mở lại bình thường
              (Dependency.Resolve<IAuthorizationService>() as ImpersonatingAuthorizationService).UndoImpersonate();
--- Code/DevSita/SIta/SIta.Web/Modules/Default/TblBags/TblBagsRow.cs
+++ Code/DevSita/SIta/SIta.Web/Modules/Default/TblBags/TblBagsRow.cs
@@ -15,7 +15,7 @@ namespace Sita.Default.Entities
     [ModifyPermission("Administration:General")]
     public sealed class TblBagsRow : Row, IIdRow, INameRow
     {
-        [DisplayName("Baggage Tag"), Column("Baggage_Tag"), Size(50), NotNull, QuickSearch]
+        [DisplayName("Baggage Tag"), Column("Baggage_Tag"), Size(50), NotNull, QuickSearch, Unique(CheckBeforeSave =true,ErrorMessage ="Bagtag đã tồn tại")]
         public String BaggageTag
         {
             get { return Fields.BaggageTag[this]; }
