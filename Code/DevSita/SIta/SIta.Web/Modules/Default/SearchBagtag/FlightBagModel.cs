5299bd879c4b2d1ee13499765c744b037da56870
nvthang02 <nvthang02@ecpay.vn>
2020-09-23 23:11:30 +0700

    add model

---
+++ Code/DevSita/SIta/SIta.Web/Modules/Default/SearchBagtag/FlightBagModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sita.Modules.Default.SearchBagtag
+{
+    public class FlightBagModel
+    {
+        public String Identify { get; set; }
+        public String Adi { get; set; }
+        public String LineCode { get; set; }
+        public String Number { get; set; }
+        public String ScheduleDate { get; set; }
+        public String SoftReplace { get; set; }
+        public String SiteIata { get; set; }
+        public String DateBoundảy { get; set; }
+        public Int32 Chute { get; set; }
+        public DateTime LastChanged { get; set; }
+        public DateTime DateCreated { get; set; }
+        public String UserCreated { get; set; }
+        public String UserUpdate { get; set; }
+        public DateTime DateUpdated { get; set; }
+        //Bagtag
+        
+        public String BaggageTag { get; set; }
+        
+        public String FlightRef { get; set; }
+        public Boolean Processed { get; set; }
+        public String Bsm { get; set; }
+        public String Bpm { get; set; }
+        
+        public DateTime TimeRcvBsm { get; set; }
+        
+        public DateTime TimeRcvBpm { get; set; }
+        
+        public Int32 Id { get; set; }
+        public String DDMM { get; set; }
+        public String YYYY { get; set; }
+    }
+}
--- Code/DevSita/SIta/SIta.Web/Modules/Default/SearchBagtag/SearchController.cs
+++ Code/DevSita/SIta/SIta.Web/Modules/Default/SearchBagtag/SearchController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Serenity.Data;
+using Sita.Modules.Default.SearchBagtag;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +16,22 @@ namespace Sita.Default.Pages
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Index(string flight, string bagtag)
+        {
+            var proName2 = "PR_SEARCH_FLIGHT_BAGTAG";
+            var connect = SqlConnections.NewByKey("Default");
+
+            var records = connect.Query<FlightBagModel>(proName2,
+                 param: new {Flight= flight,Bagtag= bagtag },
+                 commandType: System.Data.CommandType.StoredProcedure).ToList();
+
+
+            
+            if (HttpContext.Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+                return PartialView("_SearchGrid", records);
+            return View(records);
+
         }
     }
 }
