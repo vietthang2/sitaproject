2cfa0782659a66c02960cf3e17a09e87dd5d8f36
thanhphuong83 <thaithanhphuong83@gmail.com>
2020-09-23 19:36:48 +0700

    aadd

---
+++ Code/DevSita/SIta/SIta.Web/Modules/MSMQServices/ScheduleServices.cs
@@ -0,0 +1,17 @@
+﻿using Serenity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sita.Modules.MSMQServices
+{
+    public class ScheduleServices
+    {
+        public void Run()
+        {
+            new Exception("StartScheduleThread").Log();
+            Sita.Modules.MSMQServices.Schedule.StartScheduleServicesThread();
+        }
+    }
+}
