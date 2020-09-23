03c9efb367455a4962cecea153001849faa8a86b
nvthang02 <nvthang02@ecpay.vn>
2020-09-23 19:15:38 +0700

    Thang update script/ add search form

--- Code/DevSita/SIta/SIta.Web/Modules/Default/DefaultNavigation.cs
+++ Code/DevSita/SIta/SIta.Web/Modules/Default/DefaultNavigation.cs
@@ -6,3 +6,4 @@ using MyPages = Sita.Default.Pages;
 [assembly: NavigationLink(int.MaxValue, "Default/Tbl Field", typeof(MyPages.TblFieldController), icon: null)]
 [assembly: NavigationLink(int.MaxValue, "Default/Tbl Flight", typeof(MyPages.TblFlightController), icon: null)]
 [assembly: NavigationLink(int.MaxValue, "Default/Tbl Bags History", typeof(MyPages.TblBagsHistoryController), icon: null)]
+[assembly: NavigationLink(int.MaxValue, "Default/Search", typeof(MyPages.SearchController), icon: null)]
---
+++ Code/DevSita/SIta/SIta.Web/Modules/Default/SearchBagtag/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sita.Default.Pages
+{
+
+    [RoutePrefix("Default/SearchBags"), Route("{action=index}")]
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
