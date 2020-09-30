1e95305501b6e11914a15c7f30d25b75eb21dcb3
thanhphuong83 <thaithanhphuong83@gmail.com>
2020-09-30 23:07:33 +0700

    aaa

---
+++ Code/DevSita/SIta/NetCont/SearchBagTag/SearchBagTag.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SearchBagTag
+{
+    partial class SearchBagTag
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBagTag = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkActive = new System.Windows.Forms.CheckBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBagTag
+            // 
+            this.txtBagTag.Location = new System.Drawing.Point(87, 12);
+            this.txtBagTag.Name = "txtBagTag";
+            this.txtBagTag.Size = new System.Drawing.Size(196, 20);
+            this.txtBagTag.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bag Tag";
+            // 
+            // chkActive
+            // 
+            this.chkActive.AutoSize = true;
+            this.chkActive.Location = new System.Drawing.Point(87, 38);
+            this.chkActive.Name = "chkActive";
+            this.chkActive.Size = new System.Drawing.Size(56, 17);
+            this.chkActive.TabIndex = 2;
+            this.chkActive.Text = "Active";
+            this.chkActive.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(87, 61);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(196, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // SearchBagTag
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 94);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.chkActive);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBagTag);
+            this.Name = "SearchBagTag";
+            this.Text = "Search Bag Tag";
+            this.Load += new System.EventHandler(this.SearchBagTag_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBagTag;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkActive;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Code/DevSita/SIta/NetCont/SearchBagTag/SearchBagTag.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchBagTag
+{
+    public partial class SearchBagTag : Form
+    {
+       
+
+        public SearchBagTag()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchBagTag_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private string Searchbag(string pBagTag, bool pActive)
+        {
+            string Sql = "Select Chute From View_Flight_Bags Where Baggage_Tag = '" + pBagTag + "'";
+            DbConnect.OpenConnect();
+            DbConnect.glbCommand = new System.Data.OleDb.OleDbCommand(Sql,DbConnect.glbConnection);
+            Object mchute = DbConnect.glbCommand.ExecuteScalar();
+            DbConnect.CloseConnect();
+            if (mchute != null)
+            {
+                pBagTag = pBagTag + mchute.ToString();
+            }
+            else
+            {
+                pBagTag = "";
+            }
+            return pBagTag;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            Searchbag(txtBagTag.Text.ToString().Trim(), chkActive.Checked);
+
+            
+
+        }
+    }
+}
