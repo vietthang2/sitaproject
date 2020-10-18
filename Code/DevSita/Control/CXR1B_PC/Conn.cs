75ac46e5efa858b345b85c608baa0085803507c1
nvthang02 <nvthang02@ecpay.vn>
2020-10-18 21:29:09 +0700

    add Control

---
+++ Code/DevSita/Control/CXR1B_PC/Conn.cs
@@ -0,0 +1,48 @@
+﻿using CXR1BSAC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXR1B_PC
+{
+    public static class Config
+    {
+        public static string mPathServerFile = System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + @"\SearchBagTag\ConfigServer.ini";
+
+        public static string Server = ReadValue(mPathServerFile, "F", "F1");
+        public static string Database = ReadValue(mPathServerFile ,"F", "F2");
+        public static string UserName = ReadValue(mPathServerFile, "F", "F3");
+        public static string PassWord = ReadValue(mPathServerFile, "F", "F4");
+        public static string Conn = CreateConnectString(UserName, PassWord, Database, Server, 120);
+
+
+        public static string ReadValue(string pPath, string Section, string Key)
+        {
+            StringBuilder stringBuilder = new StringBuilder(255);
+            int privateProfileString = GetPrivateProfileString(Section, Key, "", stringBuilder, 255, pPath);
+            return stringBuilder.ToString();
+        }
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        public static string CreateConnectString(string pUserName, string pPassword, string pDatabase, string pServer, int pTimeOut)
+        {
+            return string.Concat(new string[]
+            {
+                "Provider=sqloledb; Data Source=",
+                pServer,
+                "; Initial Catalog=",
+                pDatabase,
+                "; User Id=",
+                pUserName,
+                "; Password=",
+                pPassword,
+                "; Connect Timeout=",
+                pTimeOut.ToString()
+            });
+        }
+    }
+}
---
+++ Code/DevSita/Control/CXR1B_PC/Logo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CXR1BSAC.Business;
+
+namespace CXR1B_PC
+{
+    public delegate void Notify();
+    public partial class Logo: UserControl
+    {
+        public event Notify ProcessCompleted;
+        string request = string.Empty;
+        string response = string.Empty;
+
+        public Logo()
+        {
+            InitializeComponent();
+        }
+
+        public string Request
+        {
+            get => request;
+            set 
+            { 
+                request = value;
+                BOBags bag = BOBags.BagsCollectionFromSearchFields(new BOBags() { BaggageTag = request }).FirstOrDefault();
+                if (bag != null)
+                {
+                    BOFlight flight = BOFlight.FlightCollectionFromSearchFields(new BOFlight() { Identify = bag.Flight }).FirstOrDefault();
+                    if(flight != null)
+                    {
+                        Response = request + (flight.Chute==null?"0": flight.Chute.ToString());
+                        Invalidate();
+                        if (ProcessCompleted != null)
+                        {
+                            ProcessCompleted();
+                        }
+                    }
+                    else
+                    {
+                        Response = request + (flight.Chute == null ? "0" : flight.Chute.ToString());
+                        Invalidate();
+                        if (ProcessCompleted != null)
+                        {
+                            ProcessCompleted();
+                        }
+                    }
+                }
+                else
+                {
+                    Response = request + "0";
+                    Invalidate();
+                    if (ProcessCompleted != null)
+                    {
+                        ProcessCompleted();
+                    }
+                }
+                Invalidate();
+            }
+        }
+
+        public string Response { get => response; set => response = value; }
+    }
+}
